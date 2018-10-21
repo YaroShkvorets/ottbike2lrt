0be55b616f75225d0c11a948d3d54495baa6aa80
zzptichka <shkvorets@gmail.com>
2018-10-21 01:53:10 -0400

    separate files for different lts, cleanup

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ const reader = require('geojson-writer').reader
 const turf = require("@turf/turf");
 const Axios = require('axios');
 
-const files = ['LRT.json'];
+const files = ['Confederation Line phase 1'];
 
 const params = {
           lng: 0,
@@ -16,41 +16,40 @@ const params = {
           units: 'kilometers',
           intervals: [3, 6, 9],
           cellSize: 0.2,
-          dir: 'lts4'
+          dir: ''
         };
 
-const result = turf.featureCollection([]);
-let places = reader('data_in/'+files[0])
-console.log("Processing file: ", files[0]);
+let places = reader(`data_in/${files[0]}.json`)
+console.log(`*** Processing file: ${files[0]}.json ***`);
 
-const promises = places.features.map(async place => {
+['lts2','lts3','lts4'].map(lts => {
+  const result = turf.featureCollection([]);
+  params.dir = lts
 
-  const name = place.properties.name;
-  console.log("Place: ", name);
-  params.lng = place.geometry.coordinates[0]
-  params.lat = place.geometry.coordinates[1]
+  const promises = places.features.map(async place => {
 
-  let url = 'https://maps.bikeottawa.ca:3000/?';
-  Object.keys(params).forEach(key => url+=key+'='+params[key]+'&');
-  Object.keys(params.intervals).forEach(i => url+='intervals='+params.intervals[i]+'&');
+    console.log(`Processing ${place.properties.name} at LTS ${lts}`);
+    params.lng = place.geometry.coordinates[0]
+    params.lat = place.geometry.coordinates[1]
 
-  const response = await Axios({
-     method: 'GET',
-     url: url,
-     json: true,
-     headers: {
-       Accept: 'application/vnd.github.v3+json',
-       'User-Agent': 'Axios'
-     }
-   })
-   console.log('response:',response.data);
-   result.features = result.features.concat(response.data.features);
+    let url = 'https://maps.bikeottawa.ca:3000/?';
+    Object.keys(params).forEach(key => url+=key+'='+params[key]+'&');
+    Object.keys(params.intervals).forEach(i => url+='intervals='+params.intervals[i]+'&');
 
+    const response = await Axios({
+       method: 'GET',
+       url: url,
+       json: true,
+       headers: {
+         Accept: 'application/vnd.github.v3+json',
+         'User-Agent': 'Axios'
+       }
+     })
+     result.features = result.features.concat(response.data.features);
+  });
 
-});
+  Promise.all(promises).then(()=>{
 
-//const results = await Promise.all(promises)
-Promise.all(promises).then(values=>{
-
-  fs.writeFileSync('data_out/'+files[0], JSON.stringify(result, null, 4));
+    fs.writeFileSync(`data_out/${files[0]}-${lts}.json`, JSON.stringify(result, null, 2));
+  });
 });
